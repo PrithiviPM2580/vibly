7414adb308235e886a2fd9b988857d35813c6530
Prithivi <prithivi@users.noreply.github.com>
2026-05-01 00:30:07 +0545

    feat: wrap RootLayout in TRPCReactProvider and update Page component to fetch AI greeting

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,10 @@ import { Geist, Geist_Mono, Inter } from "next/font/google"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
+import { TRPCReactProvider } from "@/trpc/client"
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'})
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" })
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
@@ -17,14 +18,21 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html
-      lang="en"
-      suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", inter.variable)}
-    >
-      <body>
-        <ThemeProvider>{children}</ThemeProvider>
-      </body>
-    </html>
+    <TRPCReactProvider>
+      <html
+        lang="en"
+        suppressHydrationWarning
+        className={cn(
+          "antialiased",
+          fontMono.variable,
+          "font-sans",
+          inter.variable
+        )}
+      >
+        <body>
+          <ThemeProvider>{children}</ThemeProvider>
+        </body>
+      </html>
+    </TRPCReactProvider>
   )
 }
