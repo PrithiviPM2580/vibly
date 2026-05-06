840fe1cbd0773a43493c4c75ec7897720b985884
Prithivi <prithivi@users.noreply.github.com>
2026-05-06 15:02:47 +0545

    feat: implement authentication context and middleware for enhanced security in TRPC setup

--- trpc/init.ts
+++ trpc/init.ts
@@ -1,27 +1,34 @@
-import { initTRPC } from "@trpc/server"
+import { initTRPC, TRPCError } from "@trpc/server"
 import superjson from "superjson"
-/**
- * This context creator accepts `headers` so it can be reused in both
- * the RSC server caller (where you pass `next/headers`) and the
- * API route handler (where you pass the request headers).
- */
-export const createTRPCContext = async (opts: { headers: Headers }) => {
-  // const user = await auth(opts.headers);
-  return { userId: "user_123" }
-}
-// Avoid exporting the entire t-object
-// since it's not very descriptive.
-// For instance, the use of a t variable
-// is common in i18n libraries.
-const t = initTRPC
-  .context<Awaited<ReturnType<typeof createTRPCContext>>>()
-  .create({
-    /**
-     * @see https://trpc.io/docs/server/data-transformers
-     */
-    transformer: superjson,
+import { auth } from "@clerk/nextjs/server"
+import { cache } from "react"
+
+export const createTRPCContext = cache(async () => {
+  return { auth: await auth() }
+})
+
+export type Context = Awaited<ReturnType<typeof createTRPCContext>>
+
+const t = initTRPC.context<Context>().create({
+  /**
+   * @see https://trpc.io/docs/server/data-transformers
+   */
+  transformer: superjson,
+})
+
+const isAuthed = t.middleware(({ next, ctx }) => {
+  if (!ctx.auth.userId) {
+    throw new TRPCError({ code: "UNAUTHORIZED", message: "Not authenticated" })
+  }
+
+  return next({
+    ctx: {
+      auth: ctx.auth,
+    },
   })
+})
 // Base router and procedure helpers
 export const createTRPCRouter = t.router
 export const createCallerFactory = t.createCallerFactory
 export const baseProcedure = t.procedure
+export const protectedProcedure = t.procedure.use(isAuthed)
